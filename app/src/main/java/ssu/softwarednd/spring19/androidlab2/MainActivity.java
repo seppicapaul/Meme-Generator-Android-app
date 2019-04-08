f16ea7e71ebe4f4a189d0154015e7b9de3c09920
Alonso650 <hectoralonso650@gmail.com>
2019-04-08 16:22:06 -0700

    yah

--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
@@ -1,16 +1,30 @@
 package ssu.softwarednd.spring19.androidlab2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button navigateButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigateButton = findViewById(R.id.navigate_button);
+        navigateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
+
+
 }
---
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
@@ -0,0 +1,17 @@
+package ssu.softwarednd.spring19.androidlab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class OtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
